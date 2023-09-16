86d93b0aab440ad4ba192145fdf0a79a916450a0
DarthJarJar56 <ssallam2013@gmail.com>
2023-09-16 02:56:40 -0400

    Initial backend setup stuff idk

---
+++ app.py
@@ -0,0 +1,23 @@
+# import flask and render template along with pymongo
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import os
+
+# initialize flask app and mongodb URI
+app = Flask(__name__)
+# save mongo URI
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app)
+db = mongo.cx["LadderLegendsDB"]
+
+# define app route
+@app.route('/')
+# return the render template of the hello world index
+def index():
+    user = db.Users.find_one({'name': 'helloname'})
+    print(user)
+    username = user['name'] if user else None
+    return render_template('index.html', name=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
