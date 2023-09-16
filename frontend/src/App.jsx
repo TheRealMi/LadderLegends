bc95abf7e3fa0bff7789fa16c5037a4f4df435ae
DarthJarJar56 <ssallam2013@gmail.com>
2023-09-16 11:29:54 -0400

    one more try....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState({})
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setMsg(data);
+      })
+  }, [])
 
   return (
     <>
@@ -25,6 +35,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <p>The message was {msg?.message}</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
