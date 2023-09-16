ca2af186ecc4a38cdc103e72cf91a1bebcd156f0
Angelo Magarelli <angelo.magarelli21@gmail.com>
2023-09-16 18:27:05 -0400

    Updating everything

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 import os
 
-# initialize flask app and mongodb URI
-app = Flask(__name__)
-# save mongo URI
+app = Flask(__name__, static_folder='dist', static_url_path='/')
+
+# MongoDB setup and configuration
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 # define the correct database to use
 db = mongo.cx["LadderLegendsDB"]
 
-# define app route
-@app.route('/')
-# return the render template of the hello world index
-def index():
-    user = db.Users.find_one({'name': 'myorgname'})
-    print(user)
-    username = user['name'] if user else None
-    return render_template('index.html', name=username)
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message": "hey!"})
+
+@app.route('/api/getname/<username>')
+def getname(username):
+    user = db.Users.find_one({"username": username})
+    if user:
+        return jsonify({"name": user["name"]})
+    else:
+        return jsonify({"name": "not found"})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file('index.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
