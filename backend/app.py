ac4ce06d9b147cd5afc0e0b1167a8cb35c1b1afb
DarthJarJar56 <ssallam2013@gmail.com>
2023-09-16 11:18:46 -0400

    Woohoo! Backend connected to frontend great success (testing on heroku)

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 import os
 
-# initialize flask app and mongodb URI
-app = Flask(__name__)
-# save mongo URI
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-mongo = PyMongo(app)
-# define the correct database to use
-db = mongo.cx["LadderLegendsDB"]
+app = Flask(__name__, static_folder='dist', static_url_path='/')
 
-# define app route
-@app.route('/')
-# return the render template of the hello world index
-def index():
-    user = db.Users.find_one({'name': 'myorgname'})
-    print(user)
-    username = user['name'] if user else None
-    return render_template('index.html', name=username)
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message": "hey!"})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file('index.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
