eece6366a4b31ee73f01d28e8ccf727a6f5d7058
Angelo Magarelli <angelo.magarelli21@gmail.com>
2023-09-17 05:59:42 -0400

    Added CSS and ref to Leaderboard

--- frontend/src/Components/ListBoards.tsx
+++ frontend/src/Components/ListBoards.tsx
@@ -1,41 +1,45 @@
 import { useState } from "react";
 import React from "react";
 import Leaderboard from "../Leaderboard";
-import { Route, Routes, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function ListBoards() {
-  let boards = [
-    "NLG Summer Gaming",
-    "RoK Esports Fall Ladder",
-    "Metashift League Ladder",
-  ];
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  function ListBoards() {
+    let boards = [
+      "NLG Summer Gaming",
+      "RoK Esports Fall Ladder",
+      "Metashift League Ladder",
+    ];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  return (
-    <div>
-      <h1>List of Leaderboards</h1>
-      {boards.length === 0 && (
-        <p>Looks like your leaderboards are at another Nexus</p>
-      )}
-      <ul className="list-group">
-        {boards.map((board, index) => (
-          <li
-            key={board}
-            className={
-              selectedIndex === index
-                ? "list-group-item active list-group-item-primary"
-                : "list-group-item"
-            }
-            onClick={() => {
-              setSelectedIndex(index);
-              console.log(board);
-            }}
-          >
-            {board}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-export default ListBoards;
+    return (
+      <div>
+        <div className="mx-auto">
+          <h1 className="mx-auto">Leaderboard</h1>
+        </div>
+        {boards.length === 0 && (
+          <p>Looks like your leaderboards are at another Nexus</p>
+        )}
+        <ul className="list-group">
+          {boards.map((board, index) => (
+            <li
+              key={board}
+              className={
+                selectedIndex === index
+              
+                  ? "list-group-item active list-group-item-primary"
+                  : "list-group-item"
+                  
+              }
+              onClick={() => {
+                setSelectedIndex(index);
+                console.log(board);
+              }}
+            >
+             <Link to="/Leaderboard">{board}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  export default ListBoards;
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,12 +5,15 @@ import './index.css';
 export default function Leaderboard() {
     return (
         <>
-            <div>
-            <h1 class="question">Are you a:</h1>
-            <Link to="/Components/Listboards"><button type="button" class="btn btn-primary mx-center p-2 " >Player</button></Link>
-            <Link to="/Signup"><button type="button" class="btn btn-primary mx-center p-2" >Community</button></Link> 
+        <div class="d-flex align-items-center justify-content-center">
+        <div class="p-5  m-5 bg-info text-white shadow rounded-2">
+            <div class="mx-auto">
+            <h1 class="mx-auto">Are you a:</h1>
             </div>
-          
+            <Link to="/Components/Listboards"><button type="button" class="btn btn-primary mx-3 p-2 " >Player</button></Link>
+            <Link to="/Signup"><button type="button" class="btn btn-primary mx-3 p-2" >Community</button></Link> 
+            </div>
+          </div>
         </>
     )
 
